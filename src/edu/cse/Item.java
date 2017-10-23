edc9a0803dc77b83e7c769e60af7fc0b1dfaa546
SamuelDennis <mdennis@scienceleadership.org>
2017-10-23 09:06:04 -0400

    I worked on the Item class. Please check for any errors that I have to change.

--- src/edu/cse/Item.java
+++ src/edu/cse/Item.java
@@ -1,4 +1,17 @@
 package edu.cse;
-
+//Edited By Samuel
 class Item implements Subject {
+    public String Owner;
+    public String Type;
+
+   // Item(String Owner) {Owner = name;}
+
+    String getName() { return Owner; }
+
+    public String ownerPosession() {
+    if (Owner == null) {
+        return "[GENERIC] No owner";
+    } else {
+        return "This is " + Owner + "'s " + Type; }
+    }
 }
