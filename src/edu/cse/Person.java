3eac7aa3c5e0e708d36fc819f5dd9b4220ed27b2
cancri24 <cirwindiehl@gmail.com>
2017-10-23 09:09:08 -0400

    CJ's edits day 1

--- src/edu/cse/Person.java
+++ src/edu/cse/Person.java
@@ -1,4 +1,17 @@
 package edu.cse;
 
+//edited by CJ
+import static edu.cse.Main.rand;
+
 class Person implements Subject {
+    private String name;
+    private static String[] namelist = {"Josh"};
+
+    Person() {
+        name = namelist[rand.nextInt(namelist.length)];
+    }
+
+    public String getName() {
+        return name;
+    }
 }
