0a010ac54790b9548a01ce9e534aa5f90a68b7ba
Alex <alexchuon81500@gmail.com>
2017-10-30 08:52:41 -0400

    I worked on the Main

--- src/edu/cse/Main.java
+++ src/edu/cse/Main.java
@@ -14,17 +14,36 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import java.util.Random;
 import javafx.scene.control.ChoiceBox;
+import sun.plugin.javascript.navig.Anchor;
 
 public class Main extends Application{
    // static ComboBox cb;
-    static ChoiceBox
+    static ChoiceBox cb;
     static Random rand = new Random();
-
+// Edited by alex
     @Override
     public void start(Stage firstStage) throws Exception {
 
+        cb = new ChoiceBox(FXCollections.observableArrayList("Person", "Item"));
+        cb.getSelectionModel().selectedIndexProperty().addListener(
+                new ChangeListener<Number>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                        // choicebox selected "Person"
+                        if (newValue.intValue() == 0) {
+                            // choicebox selected "Item"
+                        } else if (newValue.intValue() == 1) {
+                        }
 
-        // Alex, Creates a Combobox that lets the user choose between 3 types of pronouns
+                    }
+
+                    // Borderpane created 5 regions; top, bottom, left, right, and center
+                    BorderPane border = new BorderPane();
+                    AnchorPane topAnchorPane = new AnchorPane();
+
+
+
+                    // Alex, Creates a Combobox that lets the user choose between 3 types of pronouns
 //        cb = new ComboBox(FXCollections.observableArrayList("Male", "Female", "Other"));
 //        // Change method for the Choicebox selection
 //        cb.getSelectionModel().selectedIndexProperty().addListener(
@@ -36,12 +55,13 @@ public class Main extends Application{
 //                            //pronoun she
 //                        }else if(newValue.intValue() == 1) {
 //                            // pronoun other
-//                        }else if(newValue.intValue() == 2) {
+//                        } else if(newValue.intValue() == 2) {
 
-                        // }
-                    }
 
-        Scene theScene = new Scene(, 200, 200);
+                    //
+                });
+
+        Scene theScene = new Scene(cb, 200, 200);
         firstStage.setScene(theScene);
         firstStage.show();
 
