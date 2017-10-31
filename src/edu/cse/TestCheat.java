b97f5e13c11b10f67191e8a93ca52617f43f59d8
majestictoe <eshamble18@scienceleadership.org>
2017-10-31 10:04:56 -0400

    VictimCrime added

--- src/edu/cse/TestCheat.java
+++ src/edu/cse/TestCheat.java
@@ -3,7 +3,7 @@ package edu.cse;
 import static edu.cse.Main.rand;
 
 class TestCheat extends Crime {
-    static private String [] subject = {"biochem","algebra","advanced chem","english","physics","calculus"};
+    static private String [] subject = {"biochem","algebra","advanced chem","english","physics","calculus","driving"};
     private String mySubject;
     TestCheat(){
         mySubject = subject[rand.nextInt(subject.length-1)];
--- src/edu/cse/VictimCrime.java
+++ src/edu/cse/VictimCrime.java
@@ -1,4 +1,15 @@
 package edu.cse;
 
+import static edu.cse.Main.rand;
+
 class VictimCrime extends Crime {
+    Person victim = new Person();
+    static private String [] crime = {"ate","murdered","is in a relationship with","stabbed","burned","was seen cuddling","was seen kissing"};
+    private String cr;
+    VictimCrime(){
+        cr = crime[rand.nextInt(crime.length-1)];
+    }
+    public String whatHappened(){
+        return cr+victim.getName()+".";
+    }
 }
