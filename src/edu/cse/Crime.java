3527f422268acf6114ddd78250ab123b2228b932
majestictoe <eshamble18@scienceleadership.org>
2017-10-31 10:17:08 -0400

    Crime added

--- src/edu/cse/Crime.java
+++ src/edu/cse/Crime.java
@@ -1,5 +1,14 @@
 package edu.cse;
 
-class Crime  extends Gossip {
+import static edu.cse.Main.rand;
 
+class Crime  extends Gossip {
+    static private String [] crimes = {"smoked weed on school property","stole a laptop","cut in the lunch line","fell asleep in class","skipped class","yelled at a teacher"};
+    private String crime;
+    Crime(){
+        crime = crimes[rand.nextInt(crimes.length-1)];
+    }
+    public String whatHappened(){
+        return crime;
+    }
 }
