2378eab1bab8bc31a34e550fd2766fb49586de75
majestictoe <eshamble18@scienceleadership.org>
2017-10-31 09:52:29 -0400

    TestCheat added

--- src/edu/cse/TestCheat.java
+++ src/edu/cse/TestCheat.java
@@ -1,4 +1,14 @@
 package edu.cse;
 
+import static edu.cse.Main.rand;
+
 class TestCheat extends Crime {
+    static private String [] subject = {"biochem","algebra","advanced chem","english","physics","calculus"};
+    private String mySubject;
+    TestCheat(){
+        mySubject = subject[rand.nextInt(subject.length-1)];
+    }
+    public String whatHappened(){
+        return "cheated on a "+subject+"test";
+    }
 }
