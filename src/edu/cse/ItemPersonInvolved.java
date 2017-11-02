d05fc150a9878f593d19db873650e20e3fe88e9d
majestictoe <eshamble18@scienceleadership.org>
2017-11-02 14:32:19 -0400

    grammar fixed?

--- src/edu/cse/ItemPersonInvolved.java
+++ src/edu/cse/ItemPersonInvolved.java
@@ -5,13 +5,13 @@ import static edu.cse.Main.rand;
 
 class ItemPersonInvolved extends Gossip {
     private Person thedude;
-    static private String[] whats = {"tripped over","vandalized","ate","body slammed","smashed"};
+    static private String[] whats = {"tripped over","vandalized","eaten","body slammed","smashed"};
     private String what;
     ItemPersonInvolved(){
         thedude = new Person();
         what = whats[rand.nextInt(whats.length-1)];
     }
     public String whatHappened(){
-        return thedude.getName()+" "+what;
+        return "was "+what+" by "+thedude.getName();
     }
 }
