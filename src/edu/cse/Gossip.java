e0960a0c7c0b3e34a5736a94824cf609d0f9b694
cancri24 <cirwindiehl@gmail.com>
2017-11-02 14:53:00 -0400

    NEW PHRASES FOR YOU

--- src/edu/cse/Gossip.java
+++ src/edu/cse/Gossip.java
@@ -36,7 +36,7 @@ class Gossip {
                 fate = rand.nextInt(5);
                 subject = new Person();
                 theWord = chooseGossip(subjectType, fate);
-                return "[GENERIC] I heard that "+subject.getName()+" "+theWord.whatHappened()+" "+where+" "+when+".";
+                return Gossip.spread(subject.getName(), theWord.whatHappened(), where, when);
 
         }
         return null;
@@ -57,6 +57,24 @@ class Gossip {
                 else if (fate == 4) return new VictimCrime();
         }
         return null;
+    }
+
+    static private String spread(String who, String what, String where, String when) {
+        int phraseChoose = rand.nextInt(11);
+        if(phraseChoose == 0) return "Somebody once told me "+who+" "+what+" "+where+" "+when+".";
+        else if(phraseChoose == 1) return "Did you hear? They say "+who+" "+what+" "+where+" "+when+".";
+        else if(phraseChoose == 2) return "Rumor has it "+who+" "+what+" "+where+" "+when+".";
+        else if(phraseChoose == 3) return "A little birdy told me "+who+" "+what+" "+where+" "+when+".";
+        else if(phraseChoose == 4) return "Word around the school is that "+who+" "+what+" "+where+" "+when+".";
+        else if(phraseChoose == 5) return "Can you believe it? They're saying "+who+" "+what+" "+where+" "+when+".";
+        else if(phraseChoose == 6) return "I know it sounds crazy, but "+who+" "+what+" "+where+" "+when+".";
+        else if(phraseChoose == 7) return "Apparently "+who+" "+what+" "+where+" "+when+".";
+        else if(phraseChoose == 8) return "My sources tell me "+who+" "+what+" "+where+" "+when+".";
+        else if(phraseChoose == 9) return "Don't tell anyone I told you this, but "+who+" "+what+" "+where+" "+when+".";
+        else if(phraseChoose == 10) return "Could it be true? I heard "+who+" "+what+" "+where+" "+when+".";
+        else if(phraseChoose == 11) return "Oh my god! I was just informed that "+who+" "+what+" "+where+" "+when+".";
+        else if(phraseChoose == 12) return "This might not be true, but I've been told "+who+" "+what+" "+where+" "+when+".";
+        else return "I heard that "+who+" "+what+" "+where+" "+when+".";
     }
 
     //empty method used to resolve an error
