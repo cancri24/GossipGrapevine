0656425981e80a832dac39ea7cd8166016ec5d20
SamuelDennis <mdennis@scienceleadership.org>
2017-11-06 08:42:28 -0500

    I added more random possibilities for each class

--- src/edu/cse/Crime.java
+++ src/edu/cse/Crime.java
@@ -3,7 +3,7 @@ package edu.cse;
 import static edu.cse.Main.rand;
 
 class Crime  extends Gossip {
-    static private String [] crimes = {"smoked weed on school property","stole a laptop","cut in the lunch line","fell asleep","skipped class","yelled at a teacher"};
+    static private String [] crimes = {"smoked weed on school property","stole a laptop","cut in the lunch line","fell asleep during class","skipped class","yelled at a teacher"};
     private String crime;
     Crime(){
         crime = crimes[rand.nextInt(crimes.length-1)];
